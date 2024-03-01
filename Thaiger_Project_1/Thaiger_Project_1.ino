a1a9bde60cf04a31374ef5e92393f8e996a2ffe7
Blueheadings <56828817+Blueheadings@users.noreply.github.com>
2024-03-01 23:26:04 +0100

    Update Thaiger_Project_1.ino
    Test

--- Thaiger_Project_1/Thaiger_Project_1.ino
+++ Thaiger_Project_1/Thaiger_Project_1.ino
@@ -8,11 +8,6 @@
 #define MOTOR34_DIR_PIN 35
 #define MOTOR34_ENABLE_PIN 26
 
-// TEst 
-
-
-
-
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
